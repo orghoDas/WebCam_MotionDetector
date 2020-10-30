d647c849a8c0ea4ed85b576383539b6d582ca43c
Orgho McGuire <mcguireon@gmail.com>
2020-10-30 14:12:17 +0600

    first commit

--- script.py
+++ script.py
@@ -10,11 +10,22 @@ while True:
     check, frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    cv2.imshow("Capturing", gray)
+
+    if first_frame is None:
+        first_frame = gray
+        continue
+    
+
+    delta_frame = cv2.absdiff(first_frame, gray)
+
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Delta Frame", delta_frame)
 
     key = cv2.waitKey(1)
     print(gray)
+    print(delta_frame)
 
     if key == ord('q'):
         break
