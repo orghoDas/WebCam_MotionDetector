df668d17e127903decf772012fb779aef28c3c66
Orgho McGuire <mcguireon@gmail.com>
2020-10-30 16:42:46 +0600

    first commit

--- script.py
+++ script.py
@@ -20,8 +20,13 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray)
 
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
 
     key = cv2.waitKey(1)
     print(gray)
